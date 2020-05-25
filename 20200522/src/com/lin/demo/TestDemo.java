c82a6165d6c65e029bf8daf98d8b05d372b29b18
zhanglin268 <62052466+zhanglin268@users.noreply.github.com>
2020-05-25 23:40:48 +0800

    Add files via upload

--- 20200522/src/com/lin/demo/TestDemo.java
+++ 20200522/src/com/lin/demo/TestDemo.java
@@ -1,6 +1,8 @@
 package com.lin.demo;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Stack;
 
 /**
  * @program:20200522
@@ -72,10 +74,63 @@ public class TestDemo {
 
 
     }
+    public static void insert_sor(int[] array,int start,int end) {
+        int tmp = 0;
+        for(int i = 0;i <= end;i++) {
+            tmp = array[i];
+            int j;
+            for(j= i-1;j >= start;j--) {
+                if(array[j] > tmp) {
+                    array[j+1] = array[j];
+                }else {
+                    break;
+                }
+            }
+            array[j+1] = tmp;
+        }
+    }
+
+
+
+    //方法二：三数取中法
+    public static void swap(int a,int b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    public static void three_num_mid(int[] array,int left,int right) {
+        //array[mid] <= array[left] <= array[right]
+        int mid = (left+right)/2;
+        int tmp = 0;
+        if(array[mid] > array[left]) {
+           /* tmp = array[mid];
+            array[mid] = array[left];
+            array[right] = tmp;*/
+           swap(array[mid],array[left]);
+        }
+        if(array[mid] > array[right]) {
+
+            swap(array[mid],array[right]);
+
+        }
+        if(array[left] > array[right]) {
+           /* tmp = array[left];
+            array[left] = array[right];
+            array[right] = tmp;*/
+           swap(array[left],array[right]);
+        }
+    }
     public static void quick1(int[] array,int left,int right) {
         if(left > right) {
             return ;
         }
+        //   优化方法一：当元素个数小于一定的个数时 用直接排序
+        if(right-left+1 <= 100) {
+            insert_sor(array,left,right);
+            return ;
+        }
+
+
         int mid =  partition1(array,left,right);
         quick1(array,left,mid-1);
         quick1(array,mid+1,right);
@@ -86,6 +141,141 @@ public class TestDemo {
 
     }
     //////////////////////////////////
+
+    //非递归实现快速排序
+    public static void quickStack(int[] array) {
+        Stack<Integer> stack = stack = new Stack<>();
+        int left = 0;
+        int right = array.length-1;
+        int par = partition(array,left,right);
+        if(par > left +1) {
+            stack.push(left);
+            stack.push(par-1);
+
+        }
+        if(par < right-1) {
+            stack.push(par+1);
+            stack.push(right);
+        }
+        while(!stack.empty()) {
+            right = stack.pop();
+            left = stack.pop();
+            par = partition(array,left,right);
+            if(par > left +1) {
+                stack.push(left);
+                stack.push(par-1);
+
+            }
+            if(par < right-1) {
+                stack.push(par+1);
+                stack.push(right-1);
+            }
+        }
+    }
+
+    //归并排序
+
+    /**
+     * 时间复杂度 nlogn
+     * 空间复杂度On
+     * 稳定性:稳定
+
+     * @param array
+     * @param low
+     * @param mid
+     * @param high
+     */
+    public static void merge(int[] array,int low,int mid,int high) {
+        int s1 = low;
+        int s2 = mid+1;
+        int len = high-low+1;
+        int[] ret = new int[len];
+        int i = 0;//用来表示ret数组的下标
+        while(s1 <= mid && s2 <= high) {
+            if(array[s1] <= array[s2]) {
+                ret[i++] = array[s1++];
+
+            }else {
+                ret[i++] = array[s2++];
+            }
+        }
+        while(s1 <= mid) {
+            ret[i++] = array[s1++];
+
+        }
+        while(s2 <= high) {
+            ret[i++] = array[s2++];
+        }
+        for(int j = 0;j < ret.length;j++) {
+            array[j+low] = ret[j];
+        }
+    }
+    public static void mergeSortInternal(int[] array,int low,int high) {
+        if(low >= high) {
+            return ;
+        }
+        int mid = (low + high)/2;
+        //mid = (low + high) >>> 1;
+        mergeSortInternal(array,low,mid);
+        mergeSortInternal(array,mid+1,high);
+        merge(array,low,mid,high);
+    }
+    public static void mergeSort(int[] array) {
+        mergeSortInternal(array,0,array.length-1);
+    }
+    //归并排序的非递归实现
+    public static void mergeSort1(int[] array) {
+        for(int gap = 1;gap < array.length;gap*=2) {
+            mergeNor(array,gap);
+        }
+    }
+    public static void mergeNor(int[] array,int gap) {
+        int[] ret = new int[array.length];
+        int k = 0;//ret的下标
+        int s1 = 0;
+        int e1 = s1+gap-1;
+        int s2 = e1+1;
+        int e2 = s2+gap-1 < array.length ? s2+gap-1 :  array.length-1;
+
+        //1、肯定是有两个归并段的
+        while (s2 < array.length) {
+            //2、对应的s1位置和s2位置进行比较
+            while(s1 <= e1 && s2 <= e2) {
+                if(array[s1] <= array[s2]) {
+                    ret[k++] = array[s1];
+                    s1++;
+                }else{
+                    ret[k++] = array[s2];
+                    s2++;
+                }
+            }
+
+
+            //3、上述第2步在比较的过程当中，肯定会有一个下标先走完一个归并段
+            //4、判断是谁没走完，把剩下的数据拷贝到结果数组当中
+            while(s1 <= e1) {
+                ret[k++] = array[e1++];
+
+            }
+            while(s2 <= e2) {
+                ret[k++] = array[e2++];
+
+            }
+            //5、接着确定新的s1,e1,s2,e2
+            s1 = e2+1;
+            e1 = s1+gap-1;
+            s2 = e1+1;
+            e2 = s2+gap-1 < array.length ? s2+gap-1 :  array.length-1;
+
+            while(s1 <= array.length-1) {
+                ret[k++] = array[s1++];
+            }
+            for(int i = 0;i < ret.length;i++) {
+                array[i] = ret[i];
+            }
+        }
+    }
+
 
 
 
@@ -178,7 +368,8 @@ public class TestDemo {
 
     public static  void  shellSort(int[] array) {
 
-        int[] drr = {3,2,1};
+        //int[] drr = {3,2,1};
+        int[] drr = {5};
         for (int i = 0; i < drr.length; i++) {
             shell(array,drr[i]);
         }
@@ -258,8 +449,21 @@ public class TestDemo {
 
         }
     }
+
+    public static void main2(String[] args) {
+        int[] array = new int[100000];
+        Random random = new Random();
+        for(int i = 0;i < array.length;i++) {
+            array[i]= random.nextInt(100000);
+        }
+        long start = System.currentTimeMillis();
+        quickSort1(array);
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+
+    }
     public static void main(String[] args) {
-        int [] array = {10,3,7,5,1,9,11,15,54,78,24,48,59,8,77};
+        int [] array = {10,20,8,25,35,6,18,30,5,15,28};
 
         System.out.println(Arrays.toString(array));
         //shellSort(array);
@@ -267,8 +471,10 @@ public class TestDemo {
         //selectSort(array);
         //selectSort1(array);
         //BsInserSort(array);
-        quickSort(array);
-
+        //quickSort(array);
+        //shellSort(array);
+       // quickStack(array);
+        mergeSort1(array);
         System.out.println(Arrays.toString(array));
 
         /*System.out.println("############");
