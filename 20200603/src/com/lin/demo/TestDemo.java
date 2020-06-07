ed9db3246b6d7f5aaf14312d6fbaa17defb83935
zhanglin268 <62052466+zhanglin268@users.noreply.github.com>
2020-06-07 21:30:16 +0800

    Add files via upload

---
+++ 20200603/src/com/lin/demo/TestDemo.java
@@ -0,0 +1,26 @@
+package com.lin.demo;
+
+import java.util.Scanner;
+
+/**
+ * @program:20200603
+ * @description
+ * @author: 张林
+ * @creat:2020-06-03 01:51
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+
+        int x,y;
+        x = 5>>2;
+        y = x >>> 2;
+        System.out.println(y);
+        /*MyArrayList myArrayList = new MyArrayList();
+        for(int i = 0;i < 6;i++) {
+            myArrayList.add(i,i);
+        }
+        myArrayList.display();*/
+
+    }
+}
+Scanner scan = new Scanner()
